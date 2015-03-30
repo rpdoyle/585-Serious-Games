fc526d2b4b7280f97a9ca97d895eab4e98dae33f
Alex Parker <alex.parker215@gmail.com>
2015-03-30 16:24:15 -0400

    Created level select scene.

---
+++ Assets/Alex-working/Scripts/MenuTextControl.js
@@ -0,0 +1,33 @@
+﻿#pragma strict
+import UnityEngine.UI;
+
+public var myText:Text;
+public var myPic:GameObject;
+public var fadeTime:float;
+private var textVisible:boolean;
+
+function Start () {
+	
+}
+
+function Update () {
+	FadeText();
+}
+
+function OnMouseOver () {
+	textVisible = true;
+}
+
+function OnMouseExit () {
+	textVisible = false;
+}
+
+function FadeText() {
+	if (textVisible) {
+		myText.color = Color.Lerp (myText.color, Color.white, fadeTime * Time.deltaTime);
+		myPic.SetActive(true);
+	} else {
+		myText.color = Color.Lerp (myText.color, Color.clear, fadeTime * Time.deltaTime);
+		myPic.SetActive(false);
+	}
+}
