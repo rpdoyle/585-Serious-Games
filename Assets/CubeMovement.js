427a2f54cf4828d95494d69e472e8d3dce8738f6
Aaron Brown <aaroncbrown1993@gmail.com>
2015-03-30 12:20:22 -0400

    implemented shooting

--- Assets/CubeMovement.js
+++ Assets/CubeMovement.js
@@ -9,6 +9,14 @@ var levelLeftEdge : int = -10;
 var levelRightEdge : int = 10;
 
 
+var projectile : GameObject;
+var travelSpeed : int = 1000;
+var bulletSpawnRight : Transform;
+var bulletSpawnLeft : Transform;
+
+var right : boolean = true;
+var left : boolean = false;
+
 function Update () {
 	if (Input.GetKey(KeyCode.UpArrow) && transform.position.z < levelTopEdge) {
 		transform.Translate(Vector3(0,0,1) * Time.deltaTime*movementSpeed);
@@ -24,6 +32,9 @@ function Update () {
     	if (transform.position.x > levelLeftEdge + cameraEdge && transform.position.x < levelRightEdge - cameraEdge) {
     		mainCamera.transform.position.x = transform.position.x;
     	}
+    	
+    	right = false;
+    	left = true;
     }
     
     if (Input.GetKey(KeyCode.RightArrow) && transform.position.x < levelRightEdge) {
@@ -32,5 +43,21 @@ function Update () {
     	if (transform.position.x < levelRightEdge - cameraEdge && transform.position.x > levelLeftEdge + cameraEdge) {
     		mainCamera.transform.position.x = transform.position.x;
     	}
+    	
+    	left = false;
+    	right = true;
+    }
+    
+    
+    if(Input.GetMouseButtonDown(0) && right) {
+    	var bulletRight : GameObject = Instantiate(projectile, bulletSpawnRight.position, bulletSpawnRight.rotation);
+    	bulletRight.rigidbody.AddForce(transform.right * travelSpeed);
+    	bulletRight.rigidbody.useGravity = false;
+    }
+    
+    if(Input.GetMouseButtonDown(0) && left) {
+    	var bulletLeft : GameObject = Instantiate(projectile, bulletSpawnLeft.position, bulletSpawnLeft.rotation);
+    	bulletLeft.rigidbody.AddForce(transform.right * -travelSpeed);
+    	bulletLeft.rigidbody.useGravity = false;
     }
 } 
