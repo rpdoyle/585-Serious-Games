fed2df43eac41611c24ce0f66ff8862303215869
Ryan Doyle <rpdoyle@outlook.com>
2015-04-20 12:40:56 -0400

    Added required points functionality

---
+++ Assets/Ryan-working/RequiredPoint.js
@@ -0,0 +1,17 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+function OnTriggerEnter (object : Collider) {
+Debug.Log("triggerenter");
+	Debug.Log(object.gameObject.layer);
+	if(object.gameObject.tag == "Player") {
+		Destroy(gameObject);
+	}
+}
